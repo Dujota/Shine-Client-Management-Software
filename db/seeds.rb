18d1f418ad09048b48689059ab3c232c647e7cde
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-27 00:59:56 -0500

    created seed file using faker to create a record of 350K customers, appending the index to username and email to ensure they are unique values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+350_000.times do |seed|
+  Customer.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: "#{Faker::Internet.user.name}#{seed}",
+    email: Faker::Internet.username + seed.to_s + "@#{Faker::Internet.domain.name}"
+  )
+  print '.' if seed % 1000 == 0
+
+end
