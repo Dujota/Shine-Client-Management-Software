cd3801ebe06d50781b0e173ab8c08056d15eadec
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-29 23:48:19 -0500

    fixed typos that were giving me activeRecord errors and implemented teh search function in the index method of the customersController

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,18 @@
 class CustomersController < ApplicationController
 
   def index
-    @customers = Customer.all.limit(10)
+    if params[:keywords].present? #try any? later to see if search still works
+      @search_term = params[:keywords]
+      customer_search_term = CustomerSearchTerm.new(@search_term)
+      @customers = Customer.where(
+        customer_search_term.where_clause,
+        customer_search_term.where_args).
+        order(customer_search_term.order)
+    else
+      @customers = []
+
+    end
+    # @customers = Customer.all.limit(10)
   end
-  
+
 end
--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -20,7 +20,9 @@ private
     @where_args[:first_name] = starts_with(search_term)
 
     @where_clause << " OR #{case_insensitive_search(:last_name)}"
-    @where_args[:first_name] = starts_with(search_term)
+    @where_args[:last_name] = starts_with(search_term)
+
+    @order = "last_name asc"
   end
 
   def build_for_email_search(search_term)
