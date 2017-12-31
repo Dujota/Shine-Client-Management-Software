a3093317ee98eda2eb8642dded2345f5ed0f9ab7
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-31 12:43:27 -0500

    imported Componnt, NgModule, BrowserModule, FromsModule, platformBrowserDynamic from angular libraries

---
+++ app/javascript/packs/customers.js
@@ -0,0 +1,5 @@
+import './hello_angular/pollyfills';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
