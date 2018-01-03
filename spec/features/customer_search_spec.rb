453df1979e4195138004d7904625149a4b7b68ad
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-03 16:13:54 -0500

    created the first test user to meet devise restrictoon of tested view

--- spec/features/customer_search_spec.rb
+++ spec/features/customer_search_spec.rb
@@ -2,4 +2,13 @@ require "rails_helper"
 
 feature "Customer Search" do
 
+  def create_test_user( email: , password: )
+    User.create!(
+      email: email,
+      password: password,
+      password_confirmation: password
+    )
+  end
+
+  
 end
