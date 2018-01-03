bbfca5e82877513b56125a110e6db35f1f941b72
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-03 17:24:54 -0500

    added an *ngIf statement to section.search-results so that it will only render on the page when the @customers is displayed otherwise will get a capybara no element error on my expectations

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -6,43 +6,43 @@ import { FormsModule } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Http,HttpModule        } from "@angular/http";
 
-var RESULTS = [
-  {
-    first_name: "Pat",
-    last_name: "Smith",
-    username: "psmith",
-    email: "pat.smith@somewhere.net",
-    created_at: "2016-02-05",
-  },
-  {
-    first_name: "Patrick",
-    last_name: "Jones",
-    username: "pjpj",
-    email: "jones.p@business.net",
-    created_at: "2014-03-05",
-  },
-  {
-    first_name: "Patricia",
-    last_name: "Benjamin",
-    username: "pattyb",
-    email: "benjie@aol.info",
-    created_at: "2016-01-02",
-  },
-  {
-    first_name: "Patty",
-    last_name: "Patrickson",
-    username: "ppat",
-    email: "pppp@freemail.computer",
-    created_at: "2016-02-05",
-  },
-  {
-    first_name: "Jane",
-    last_name: "Patrick",
-    username: "janesays",
-    email: "janep@company.net",
-    created_at: "2013-01-05",
-  },
-];
+// var RESULTS = [
+//   {
+//     first_name: "Pat",
+//     last_name: "Smith",
+//     username: "psmith",
+//     email: "pat.smith@somewhere.net",
+//     created_at: "2016-02-05",
+//   },
+//   {
+//     first_name: "Patrick",
+//     last_name: "Jones",
+//     username: "pjpj",
+//     email: "jones.p@business.net",
+//     created_at: "2014-03-05",
+//   },
+//   {
+//     first_name: "Patricia",
+//     last_name: "Benjamin",
+//     username: "pattyb",
+//     email: "benjie@aol.info",
+//     created_at: "2016-01-02",
+//   },
+//   {
+//     first_name: "Patty",
+//     last_name: "Patrickson",
+//     username: "ppat",
+//     email: "pppp@freemail.computer",
+//     created_at: "2016-02-05",
+//   },
+//   {
+//     first_name: "Jane",
+//     last_name: "Patrick",
+//     username: "janesays",
+//     email: "janep@company.net",
+//     created_at: "2013-01-05",
+//   },
+// ];
 
 var CustomerSearchComponent = Component({
   selector: "shine-customer-search",
@@ -60,7 +60,7 @@ var CustomerSearchComponent = Component({
            class="form-control input-lg">\
   </form> \
 </section> \
-<section class="search-results"> \
+<section class="search-results" *ngIf="customers"> \
   <header> \
     <h1 class="h3">Results</h1> \
   </header> \
