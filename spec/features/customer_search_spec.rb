1e1013331c42dfabcb89510927e3c4ecdea8f37e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-03 16:57:34 -0500

    created the first acceptance scenario for capybara to execute a name only search after it logs into the app as the user

--- spec/features/customer_search_spec.rb
+++ spec/features/customer_search_spec.rb
@@ -20,11 +20,51 @@ feature "Customer Search" do
       username: username,
       email: email
     )
-
   end
 
   let(:email)     { "pat@example.com" }
   let(:password)  { "password123" }
+
+  before do
+    create_test_user(email: email, password: password)
+
+    create_customer( first_name: "Chris", last_name: "Aaron")
+    create_customer( first_name: "Pat", last_name: "Johnson")
+    create_customer( first_name: "I.T.", last_name: "Pat")
+    create_customer( first_name: "Patricia", last_name: "Dobbs")
+
+    # This user is the one we'll expect to be the first listed
+    create_customer(
+      first_name: "Pat",
+      last_namne: "Jones",
+      email: "pat123@somewhere.net"
+    )
+  end
+
+  scenario " Search by Name" do
+    visit "/custoemrs"
+
+    #Login with user to get access to /customers
+
+    fill_in "Email",      with: email
+    fill_in "Password",   with: password
+    click_button "Log in"
+
+    within "section.search-form" do
+      fill_in "keywords", with: "pat"
+    end
+
+    within "section.search-results" do
+      expect(page).to have_content("Results")
+      expect(page.all("ol.li.list-group-item").count).to eq(4)
+
+      list_group_items = page.all("ol.li.list-group-item")
+      expect(list_group_item[0]).to have_content("Patricia")
+      expect(list_group_item[0]).to have_content("Dobbs")
+      expect(list_group_item[3]).to have_conent("I.T.")
+      expect(list_group_item[3]).to have_conent("Pat")
+    end
+  end
 
 
 end
