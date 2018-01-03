2eed7ed95f13edbb15a9335a6dd49291d016e8f1
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-03 16:33:48 -0500

    created all the data required to run the testing and made one user that will meet all search criteria so we can test if the sorting works as expected ontop of the searching itself

--- spec/features/customer_search_spec.rb
+++ spec/features/customer_search_spec.rb
@@ -10,5 +10,21 @@ feature "Customer Search" do
     )
   end
 
-  
+  def create_customer(first_name:, last_name:, email: nil)
+    username = "#{Faker::Internet.user_name}#{rand(1000)}"
+    email ||= "#{username}#{rand(1000)}@" + "#{Faker::Internet.domain_name}"
+
+    Customer.create!(
+      first_name: first_name,
+      last_name: last_name,
+      username: username,
+      email: email
+    )
+
+  end
+
+  let(:email)     { "pat@example.com" }
+  let(:password)  { "password123" }
+
+
 end
