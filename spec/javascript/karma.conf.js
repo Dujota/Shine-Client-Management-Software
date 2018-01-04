4e08f7411591a58a26503c42bf512d3901496919
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-04 15:49:42 -0500

    updated dependencies of karama and jasmine, ran yarn install and fixed error in the karma conf file, ran a test spec to see if is configured properly and sucessfully ran test

--- spec/javascript/karma.conf.js
+++ spec/javascript/karma.conf.js
@@ -1,11 +1,6 @@
-module.exports - function(config) {
-  config.set({
-    frameworks: ['jasmine'],
-    files: ['**/*.spec.js'],
-    preprocessors: {
-      '**/*.spec.js': ['webpack']
-    },
-    webpack: require('../../config/webpack/test.js'),
-    browsers: ['PhantomJS']
-  })
-}
+module.exports = function(config) { config.set({
+frameworks: ['jasmine'],
+files: ['**/*.spec.js'],
+preprocessors: {'**/*.spec.js': [ 'webpack' ] },
+webpack: require('../../config/webpack/test.js'),
+browsers: ['PhantomJS'] }) }
