64e3b437b2ed8ad797fee6d8b56aded3f69b0194
Denis Sanchez Dujota <e_scriba@hotmail.com>
2018-01-05 13:56:07 -0500

    moved the angular component to its own file and moved the dependency injection to the component, imported component in the customers.js file that webpack will serve up, I can now test the angular component on its own

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -2,11 +2,13 @@ import 'hello_angular/polyfills';
 import "babel-polyfill";
 
 
-import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule               } from '@angular/core';
+import { BrowserModule          } from '@angular/platform-browser';
+import { FormsModule            } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Http,HttpModule        } from "@angular/http";
+import { HttpModule             } from "@angular/http";
+import { CustomerSearchComponent} from "CustomerSearchComponent";
+
 
 // var RESULTS = [
 //   {
@@ -45,70 +47,6 @@ import { Http,HttpModule        } from "@angular/http";
 //     created_at: "2013-01-05",
 //   },
 // ];
-
-var CustomerSearchComponent = Component({
-  selector: "shine-customer-search",
-  template: '\
-<header> \
-  <h1 class="h2">Customer Search</h1> \
-</header> \
-<section class="search-form"> \
-  <form> \
-    <label for="keywords" class="sr-only">Keywords></label> \
-    <input type="text" id="keywords" name="keywords" \
-           placeholder="First Name, Last Name, or Email Address"\
-           bindon-ngModel="keywords" \
-           on-ngModelChange="search($event)" \
-           class="form-control input-lg">\
-  </form> \
-</section> \
-<section class="search-results" *ngIf="customers"> \
-  <header> \
-    <h1 class="h3">Results</h1> \
-  </header> \
-  <ol class="list-group"> \
-    <li *ngFor="let customer of customers" \
-      class="list-group-item clearfix"> \
-      <h3 class="pull-right"> \
-        <small class="text-uppercase">Joined</small> \
-        {{customer.created_at}} \
-      </h3> \
-      <h2 class="h3"> \
-        {{customer.first_name}} {{customer.last_name}} \
-        <small>{{customer.username}}</small> \
-      </h2> \
-      <h4>{{customer.email}}</h4> \
-    </li> \
-  </ol> \
-</section> \
-  '
-}).Class({
-  constructor: [
-    Http,
-    function(http) {
-    this.customers  = null;
-    this.http       = http;
-    this.keywords   = "";
-    }
-  ],
-  search: function($event) {
-  var self = this; // saving this (search) into local variable so we can refer to the same thing throughout the code as "this" will change depending where we are.
-  self.keywords = $event
-  if (self.keywords.length < 3) {
-    return;
-  }
-  self.http.get(
-    "/customers.json?keywords=" + self.keywords // this is the url to our rails controller with keywords in params
-  ).subscribe( // observable RxJS wait for http to finish then run functions in the subscribe
-    function(response) {
-      self.customers = response.json().customers; // extracts results from response and sets it to customers
-    },
-    function(response) {
-      window.alert(response); //error handling in case the http request fails
-    }
-  );
-  }
-});
 
 var CustomerAppMpdule = NgModule({
   imports: [BrowserModule, FormsModule, HttpModule],
